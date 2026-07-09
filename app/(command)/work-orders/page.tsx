e2b9a782b343dc32c8d0151a78476ec1f58cccd8
gabhrielfiore <gabhrielfiore@gmail.com>
2026-07-09 01:59:35 -0300

    Render work orders page dynamically

--- app/(command)/work-orders/page.tsx
+++ app/(command)/work-orders/page.tsx
@@ -1,6 +1,8 @@
 import { WorkOrdersModule } from "@/modules/work-orders/components/work-orders-module";
 import { listWorkOrders } from "@/services/work-orders";
 
+export const dynamic = "force-dynamic";
+
 export default async function WorkOrdersPage() {
   const workOrders = await listWorkOrders();
 
