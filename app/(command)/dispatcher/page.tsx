af1a026ef61f80ed3916e4a07e81cbf2ae4f1c6c
gabhrielfiore <gabhrielfiore@gmail.com>
2026-07-09 02:24:06 -0300

    Connect dispatcher page to service

--- app/(command)/dispatcher/page.tsx
+++ app/(command)/dispatcher/page.tsx
@@ -1,5 +1,10 @@
 import { DispatcherModule } from "@/modules/dispatcher/components/dispatcher-module";
+import { listDispatcherJobs } from "@/services/dispatcher";
 
-export default function DispatcherPage() {
-  return <DispatcherModule />;
+export const dynamic = "force-dynamic";
+
+export default async function DispatcherPage() {
+  const jobs = await listDispatcherJobs();
+
+  return <DispatcherModule jobs={jobs} />;
 }
