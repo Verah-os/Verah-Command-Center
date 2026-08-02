26034c24475cee362307ee56b7f65d771621bcb3
gabhrielfiore <299614142+gabhrielfiore@users.noreply.github.com>
2026-08-02 01:34:05 -0300

    feat(quote-intelligence): add secure classifier adapter

---
+++ services/quote-intelligence/index.ts
@@ -0,0 +1,5 @@
+export { buildQuoteIntelligenceInput } from "./input";
+export { createQuoteIntelligenceLog } from "./observability";
+export { classifyQuoteIntelligence } from "./repository";
+export type * from "./types";
+
